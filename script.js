c9ba89d98dedb44d88498fce1b3492fe11d4046d
Sabrina-Andrade-Lopes <sabrina.a.a.lopes1110@gmail.com>
2026-03-18 23:52:29 -0300

    ajuste_menu_mobile

--- script.js
+++ script.js
@@ -1,51 +1,57 @@
-// Captura o formulário
+// FORMULÁRIO
 document.getElementById("formContato").addEventListener("submit", function(event){
 
 event.preventDefault();
 
-// Pegando valores
 let nome = document.getElementById("nome").value;
 let email = document.getElementById("email").value;
 let mensagem = document.getElementById("mensagem").value;
 
-//validar email
 let emailValido = /\S+@\S+\.\S+/;
 
-// Verifica campos vazios
 if(nome == "" || email == "" || mensagem == ""){
-
 alert("Preencha todos os campos!");
 return;
-
 }
 
-// Verifica email
 if(!emailValido.test(email)){
-
 alert("Digite um email válido!");
 return;
-
 }
 
-// Simulação de envio
 alert("Mensagem enviada com sucesso!");
-
-// Limpa formulário
 document.getElementById("formContato").reset();
 
 });
 
-// BOTÃO DE TEMA
 
+// TEMA ESCURO
 let botaoTema = document.getElementById("temaBtn");
+
 botaoTema.addEventListener("click", function(){
+
 document.body.classList.toggle("dark-mode");
+
 if(document.body.classList.contains("dark-mode")){
 botaoTema.textContent = "☀️ Tema claro";
 }else{
-
 botaoTema.textContent = "🌙 Tema escuro";
-
 }
 
+});
+
+
+// MENU MOBILE
+let menuToggle = document.getElementById("menuToggle");
+let menu = document.getElementById("menu");
+
+menuToggle.addEventListener("click", function(){
+menu.classList.toggle("active");
+});
+
+// FECHAR MENU AO CLICAR
+document.querySelectorAll("#menu a").forEach(link => {
+link.addEventListener("click", () => {
+menu.classList.remove("active");
+});
 });
